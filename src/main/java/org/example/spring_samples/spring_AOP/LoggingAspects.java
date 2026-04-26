45fe63c9cf59190c3dac9b2380596ae872e61350
Navitha214 <navithaadhoni2114@gmail.com>
2026-04-26 22:18:49 +0530

    ebox

--- src/main/java/org/example/spring_samples/spring_AOP/LoggingAspects.java
+++ src/main/java/org/example/spring_samples/spring_AOP/LoggingAspects.java
@@ -16,28 +16,28 @@ public class LoggingAspects {
 
     @Before(" execution(* org.example.spring_samples.spring_AOP.PaymentService.*(..))")
     public void logBefore(JoinPoint jp){
-        String name = jp.getSignature().getName();
+        String id = jp.getSignature().getName();
         Object[] args = jp.getArgs();
-        System.out.println("Logging Before payment method execution. MethodName: "+name + "  args:  "+ Arrays.asList(args));
+        System.out.println("Logging Before payment method execution. MethodId: "+id + "  args:  "+ Arrays.asList(args));
     }
 
     @After(" execution(* org.example.spring_samples.spring_AOP.PaymentService.*(..))")
     public void logAfter(JoinPoint jp){
-        String name = jp.getSignature().getName();
+        String id = jp.getSignature().getName();
         Object[] args = jp.getArgs();
-        System.out.println("Logging After payment method execution. MethodName: "+name + "  args:  "+ Arrays.asList(args));
+        System.out.println("Logging After payment method execution. MethodId: "+id + "  args:  "+ Arrays.asList(args));
     }
 
     @Around(" execution(* org.example.spring_samples.spring_AOP.PaymentService.*(..))")
     public Object logAround(ProceedingJoinPoint jp) throws Throwable{
-        String name = jp.getSignature().getName();
+        String id = jp.getSignature().getName();
         Object[] args = jp.getArgs();
-        System.out.println("Logging After payment method execution. MethodName: "+name + "  args:  "+ Arrays.asList(args));
+        System.out.println("Logging After payment method execution. MethodId: "+id + "  args:  "+ Arrays.asList(args));
         Long startTime = System.currentTimeMillis();
         Object result = jp.proceed();
         Long endTime = System.currentTimeMillis();
         System.out.println("Execution Time : "+(endTime - startTime));
-        System.out.println("Logging After payment method execution. MethodName: "+name + "  args:  "+ Arrays.asList(args) + " Result: "+result);
+        System.out.println("Logging After payment method execution. MethodId: "+id + "  args:  "+ Arrays.asList(args) + " Result: "+result);
         return result;
     }
 }
